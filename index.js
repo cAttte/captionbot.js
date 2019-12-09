a5f98f17f181b2c7546d541bf78a4a9e6fc85403
Santiago-MG <santiagomontenegrogerik@gmail.com>
2019-12-08 21:41:24 -0300

    Added all the package files

---
+++ index.js
@@ -0,0 +1,71 @@
+const request = require("request")
+
+exports.caption = (imageURL, callback) => {
+
+    const options = {
+
+        "url": "https://captionbot.azurewebsites.net/api/messages",
+
+        "method": "POST",
+
+        "headers": {
+
+            "Content-Type": "application/json; charset=utf-8"
+
+        },
+
+        "json": {
+
+            "Content": imageURL,
+            "Type": "CaptionRequest"
+
+        }
+
+    }
+
+    function handleErrors(response) {
+
+        if (response.includes("Did you upload an image?")) {
+
+            return "Image URL not specified."
+
+        } else if (response === "I really can't describe the picture 😳"
+                || response === "I'm not sure what you're asking") {
+
+            return "Invalid Image URL."
+
+        }
+
+    }
+
+    if (!callback) {
+
+        return new Promise((resolve, reject) => {
+
+            request(options, (err, res, body) => {
+
+                if (err) throw err
+                var error = handleErrors(res.body)
+                if (error) reject(error)
+
+                resolve(res.body)
+
+            })
+
+        })
+
+    } else {
+
+        request(options, (err, res, body) => {
+
+            if (err) throw err
+            var error = handleErrors(res.body)
+            if (error) throw new Error(error)
+
+            callback(res.body)
+
+        })
+
+    }
+
+}
