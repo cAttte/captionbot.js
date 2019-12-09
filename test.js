e77e22e1127be491b17c217206c5e0756b5f36b5
Santiago-MG <santiagomontenegrogerik@gmail.com>
2019-12-08 22:09:26 -0300

    Added all the package's files and updated README

--- test.js
+++
@@ -1,8 +0,0 @@
-const caption = require("./index.js").caption
-var problematicURLs = [null, undefined, ""]
-
-var imageURL = problematicURLs[Math.floor(Math.random() * problematicURLs.length)]
-
-caption(imageURL)
-    .then((caption) => console.log(caption))
-    .catch((error) => console.log("ERROR: " + error))
