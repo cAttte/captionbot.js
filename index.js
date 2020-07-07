8ab15a5da1c1ba2f2186f6d13d29824e552f950a
cAttte <26514199+cAttte@users.noreply.github.com>
2020-07-07 15:44:09 -0300

    why are there so many line breaks

--- index.js
+++ index.js
@@ -3,69 +3,42 @@ const request = require("request")
 exports.caption = (imageURL, callback) => {
 
     const options = {
-
         "url": "https://captionbot.azurewebsites.net/api/messages",
-
         "method": "POST",
-
         "headers": {
-
             "Content-Type": "application/json; charset=utf-8"
-
         },
-
         "json": {
-
             "Content": imageURL,
             "Type": "CaptionRequest"
-
         }
-
     }
 
     function handleErrors(response) {
-
         if (response.includes("Did you upload an image?")) {
-
             return "Image URL not specified."
-
         } else if (response === "I really can't describe the picture 😳"
                 || response === "I'm not sure what you're asking") {
-
             return "Invalid Image URL."
-
         }
-
     }
 
     if (!callback) {
-
         return new Promise((resolve, reject) => {
-
             request(options, (err, res, body) => {
-
                 var error = err || handleErrors(res.body)
                 if (error) reject(error)
-
                 resolve(res.body)
-
             })
-
         })
-
     } else {
-
         request(options, (err, res, body) => {
-
             var error = err || handleErrors(res.body)
             if (error) {
                 callback(error)
             } else {
                 callback(error, res.body)
             }
-
         })
-
     }
-
 }
